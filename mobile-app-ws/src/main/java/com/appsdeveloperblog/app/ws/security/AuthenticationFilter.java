87512a798cb7dbc40f26b3c843f53eb2468d2049
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-02-04 11:58:17 +0000

    added new security models

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -1,5 +1,42 @@
 package com.appsdeveloperblog.app.ws.security;
 
-public class AuthenticationFilter {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.appsdeveloperblog.app.ws.ui.model.request.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private final AuthenticationManager authenticationManager;
+
+	public AuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req,
+												HttpServletResponse res) throws AuthenticationException {
+		try {
+			UserLoginRequestModel creds = new ObjectMapper()
+					.readValue(req.getInputStream(), UserLoginRequestModel.class);
+
+			return authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							creds.getEmail(),
+							creds.getPassword(),
+							new ArrayList<>())
+			);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
