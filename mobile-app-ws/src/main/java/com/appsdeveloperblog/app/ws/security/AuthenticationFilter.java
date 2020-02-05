cda9bc3b0903b0ffee9fa24f6f6f5e78f71da6e8
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-02-05 13:44:58 +0000

    AuthenticationFilter added

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.appsdeveloperblog.app.ws.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,10 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.appsdeveloperblog.app.ws.ui.model.request.UserLoginRequestModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	private final AuthenticationManager authenticationManager;
@@ -38,5 +45,22 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req,
+											HttpServletResponse res,
+											FilterChain chain,
+											Authentication auth) throws IOException, ServletException {
+		String userName = ((User) auth.getPrincipal()).getUsername();
+//		String tokenSecret = new SecurityConstants().getTokenSecret();
+		
+		String token = Jwts.builder()
+				.setSubject(userName)
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+				.compact();
+		
+		res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
 	}
 }
--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/shared/Utils.java
@@ -21,6 +21,7 @@ public class Utils {
 		for (int i = 0; i <length; i++) {
 			returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
 		}
+	
 		
 		return new String(returnValue);
 	}
