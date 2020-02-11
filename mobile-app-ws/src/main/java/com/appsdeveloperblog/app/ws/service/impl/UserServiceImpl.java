fff450bbe60a29bda2892097ca4fc8d6354e9622
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-02-11 13:54:44 +0000

    fixing UserServiceImplementation

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -74,7 +74,11 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserDto getUserByUserId(String userId) {
 		UserDto returnValue = new UserDto();
-		UserEntity userEntity = userRepository.findByUserId();
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(userId);
+		
+		BeanUtils.copyProperties(userEntity, returnValue);
 		
 		return null;
 	}
